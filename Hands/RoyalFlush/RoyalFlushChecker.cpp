92c49fa2cd07581767260294772230380d77838f
HafizAFauzi <139345624+HafizAFauzi@users.noreply.github.com>
2026-04-06 10:05:29 +0700

    Fix hands "RoyalFlush"
    Modifikasi return fungsi check

--- Hands/RoyalFlush/RoyalFlushChecker.cpp
+++ Hands/RoyalFlush/RoyalFlushChecker.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "RoyalFlushChecker.h"
 
-bool RoyalFlushChecker::check(const Hand &hand)
+HandRank RoyalFlushChecker::check(const Hand &hand)
 {
     if (isRoyalFlush(hand))
     {
         std::cout << "Detected ROYAL FLUSH\n";
-        return true;
+        return HandRank::ROYAL_FLUSH;
     }
-    return false;
+    return HandRank::HIGH_CARD;
 }
 
 bool RoyalFlushChecker::isRoyalFlush(const Hand &hand)
