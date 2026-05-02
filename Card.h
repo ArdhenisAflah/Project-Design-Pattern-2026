8b485eb88ece34c3a2d7bc327c128303d7694041
ArdhenisAflah <aflahdenis9@gmail.com>
2026-05-02 22:02:38 +0700

    Add Card and 2 Enums to support functionality
    - Add Card,h -> for structure of each card that will be played and generated
    - Add Rank.h -> as reference for Card.h to determine Rank (as well others classes in the future)
    - Add Suit.h -> as reference for Card.h to determine Suit ( as well others classes in the fucure)

---
+++ Card.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Suit.h"
+#include "Rank.h"
+struct Card
+{
+    Rank rank;
+    Suit suit;
+};
---
+++ Rank.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum class Rank
+{
+    TWO = 2,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+};
