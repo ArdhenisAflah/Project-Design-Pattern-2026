bb0f685d91774c4bb443e40320fc5f0add636569
ArdhenisAflah <aflahdenis9@gmail.com>
2026-05-02 22:17:50 +0700

    Add & Refactor functionality HandPlayer (.h) & (.cpp)
    - Modify dummy content, with real logic to handle choosed hand played by player.
    - Add input cards functionality

--- HandPlayer.cpp
+++ HandPlayer.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include "HandPlayer.h"
-void HandPlayer::playHand()
+void HandPlayer::playHand(Hand &hand)
 {
-    std::cout << "Player selects cards to play...\n";
+    std::cout << "Pilih indeks kartu (pisahkan dengan spasi, akhiri dengan -1):";
+
+    int choice;
+
+    while (std::cin >> choice && choice != -1)
+    {
+        if (choice >= 0 && choice < hand.cards.size())
+        {
+            this->choosenCards.push_back(hand.cards[choice]);
+        }
+        else
+        {
+            std::cout << "Indeks tidak valid!" << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
 }
--- HandPlayer.h
+++ HandPlayer.h
@@ -1,6 +1,18 @@
 #pragma once
+#include "Hand.h"
+#include <vector>
 class HandPlayer
 {
+
+private:
+    std::vector<Card> choosenCards;
+
 public:
-    void playHand();
+    void playHand(Hand &hand);
+    // getter function
+    Hand getChoosenHand() const
+    {
+        Hand handChoosed{1, choosenCards};
+        return handChoosed;
+    }
 };
