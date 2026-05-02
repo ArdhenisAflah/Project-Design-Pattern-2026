580258ae59075e27b3f7d7b39b883dcd5660efcc
ArdhenisAflah <aflahdenis9@gmail.com>
2026-05-02 22:19:41 +0700

    Refactoring & Add functionality GameManager.cpp
    - Add display card generator and played functionality
    - Changes scoreHand parameter to real choosedHand (contains hand that actually played by player)

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,11 +1,151 @@
 #include <iostream>
+#include <string>
 #include "GameManager.h"
 void GameManager::runSession()
 {
     std::cout << "=== Run Started ===\n";
     Hand hand = handGenerator.generateHand();
-    handPlayer.playHand();
-    int score = scoringRule.scoreHand(hand);
+    std::cout << "HAND GENERATED: " << std::endl;
+
+    std::cout << "=======================" << std::endl;
+    for (const auto &s : hand.cards)
+    {
+        std::string rankConverted;
+        std::string suitConverted;
+
+        switch (s.suit)
+        {
+        case Suit::HEARTS:
+            suitConverted = "H";
+            break;
+        case Suit::DIAMONDS:
+            suitConverted = "D";
+            break;
+        case Suit::CLUBS:
+            suitConverted = "C";
+            break;
+        case Suit::SPADES:
+            suitConverted = "S";
+            break;
+        }
+        switch (s.rank)
+        {
+        case Rank::ACE:
+            rankConverted = "A";
+            break;
+        case Rank::TWO:
+            rankConverted = "2";
+            break;
+        case Rank::THREE:
+            rankConverted = "3";
+            break;
+        case Rank::FOUR:
+            rankConverted = "4";
+            break;
+        case Rank::FIVE:
+            rankConverted = "5";
+            break;
+        case Rank::SIX:
+            rankConverted = "6";
+            break;
+        case Rank::SEVEN:
+            rankConverted = "7";
+            break;
+        case Rank::EIGHT:
+            rankConverted = "8";
+            break;
+        case Rank::NINE:
+            rankConverted = "9";
+            break;
+        case Rank::TEN:
+            rankConverted = "10";
+            break;
+        case Rank::JACK:
+            rankConverted = "J";
+            break;
+        case Rank::QUEEN:
+            rankConverted = "Q";
+            break;
+        case Rank::KING:
+            rankConverted = "K";
+            break;
+        }
+        std::cout << rankConverted << "::" << suitConverted << std::endl;
+    }
+    std::cout << "=======================" << std::endl;
+
+    handPlayer.playHand(hand);
+    Hand choosedHand = handPlayer.getChoosenHand();
+
+    std::cout << "YOU CHOOSED : " << std::endl;
+    std::cout << "=======================" << std::endl;
+    for (const auto &s : choosedHand.cards)
+    {
+        std::string rankConverted;
+        std::string suitConverted;
+
+        switch (s.suit)
+        {
+        case Suit::HEARTS:
+            suitConverted = "H";
+            break;
+        case Suit::DIAMONDS:
+            suitConverted = "D";
+            break;
+        case Suit::CLUBS:
+            suitConverted = "C";
+            break;
+        case Suit::SPADES:
+            suitConverted = "S";
+            break;
+        }
+        switch (s.rank)
+        {
+        case Rank::ACE:
+            rankConverted = "A";
+            break;
+        case Rank::TWO:
+            rankConverted = "2";
+            break;
+        case Rank::THREE:
+            rankConverted = "3";
+            break;
+        case Rank::FOUR:
+            rankConverted = "4";
+            break;
+        case Rank::FIVE:
+            rankConverted = "5";
+            break;
+        case Rank::SIX:
+            rankConverted = "6";
+            break;
+        case Rank::SEVEN:
+            rankConverted = "7";
+            break;
+        case Rank::EIGHT:
+            rankConverted = "8";
+            break;
+        case Rank::NINE:
+            rankConverted = "9";
+            break;
+        case Rank::TEN:
+            rankConverted = "10";
+            break;
+        case Rank::JACK:
+            rankConverted = "J";
+            break;
+        case Rank::QUEEN:
+            rankConverted = "Q";
+            break;
+        case Rank::KING:
+            rankConverted = "K";
+            break;
+        }
+        std::cout << rankConverted << "::" << suitConverted << std::endl;
+    }
+    std::cout << "=======================" << std::endl;
+
+    int score = scoringRule.scoreHand(choosedHand);
     bool win = blindRule.checkBlind(score);
     int reward = rewardRule.earnMoney(win, score);
     std::cout << "Money gained: " << reward << "\n";
